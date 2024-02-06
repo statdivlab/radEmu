299c50f572b0fe5bb17a101f810702d40cb2b65d
Sarah Teichman <teichs@uw.edu>
2024-02-06 15:04:37 -0800

    updating the `print.emuFit` method to print data frame, label the categories and covariates if they are not labeled in the data, and print appropriate number of digits for integers

--- R/emuFit.R
+++ R/emuFit.R
@@ -425,6 +425,13 @@ and the corresponding gradient function to constraint_grad_fn.")
     }
   }
   
+  if (is.null(colnames(X))) {
+    colnames(X) <- c("Intercept", paste0("covariate_", 1:(ncol(X) - 1)))
+  }
+  if (is.null(colnames(Y))) {
+    colnames(Y) <- paste0("category_", 1:ncol(Y))
+  }
+  
   if (!is.null(colnames(X))) {
     if (length(unique(colnames(X))) == ncol(X)) {
       k_to_covariates <- data.frame(k = 1:p,
