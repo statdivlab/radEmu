be9becf16a432d09729f9599683957eddf747510
Sarah Teichman <teichs@uw.edu>
2024-06-10 10:16:47 -0700

    update `simulate_data()` function so that it will work with `B` with arbitrary number of rows `p`

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -11,6 +11,7 @@
 #' @param zinb_zero_prop Proportion of zeros for ZINB data
 #' @param mean_count_before_ZI Parameter for construction of z_i in mean model. Setting this to \code{50} works well in practice. 
 #' @param X Optional design matrix, this must have two columns and n rows.
+#' @param B Optional B matrix, if p is not equal to 2 
 #' @param cluster Optional cluster vector, this must have n elements. 
 #'
 #' @return \code{Y}. A \code{n times J} dimension matrix of simulated response counts.
@@ -20,19 +21,29 @@
 #' @export
 simulate_data <- function(n,
                           J,
-                          b0,
-                          b1,
+                          b0 = NULL,
+                          b1 = NULL,
                           distn,
                           zinb_size = NULL,
                           zinb_zero_prop = NULL,
                           mean_count_before_ZI,
                           X = NULL,
+                          B = NULL,
                           cluster = NULL) {
   
   if (is.null(X)) {
     X <- cbind(1,rep(c(0,1),each = n/2))
   }
-  B <- rbind(b0,b1)
+  if (!is.null(b0) & !is.null(b1)) {
+    B <- rbind(b0,b1)
+    if (nrow(B) != ncol(X)) {
+      stop("You've input b0 and b1 but your X matrix does not have 2 columns. Please use the B argument when your design matrix does not have 2 columns.")
+    }
+  } else if (is.null(b0) | is.null(b1)) {
+    if (is.null(B)) {
+      stop("Please input either parameter vectors b0 and b1, or parameter matrix B.")
+    }
+  }
   log_means <- do.call(cbind,
                        lapply(1:J,
                               function(j) X%*%B[,j,drop = FALSE]))
