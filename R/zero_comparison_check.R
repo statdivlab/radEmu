abf7aeb83dddea39335459174cf9bfa66017b5d6
Sarah Teichman <teichs@uw.edu>
2025-04-25 11:33:26 -0700

    patch to zero_comparison_check function

--- R/zero_comparison_check.R
+++ R/zero_comparison_check.R
@@ -17,36 +17,41 @@ zero_comparison_check <- function(X, Y) {
       for (col in cat_covs) {
         base_X <- X[, col_assign == col]
         
-        # get indices for each group 
-        n_groups <- ncol(base_X) + 1
-        group_ind <- vector(mode = "list", length = n_groups)
-        group_ind[[1]] <- which(rowSums(base_X) == 0)
+        all_0_1 <- sum(!(base_X %in% c(0, 1))) == 0
+        sum_0_or_1 <- sum(!(rowSums(base_X) %in% c(0, 1))) == 0
         
-        for (i in 1:ncol(base_X)) {
-          group_ind[[i + 1]] <- which(base_X[, i] == 1)
-        }
-        
-        # get Y sums for each group 
-        J <- ncol(Y)
-        group_counts <- matrix(NA, nrow = n_groups, ncol = J)
-        for (i in 1:n_groups) {
-          group_counts[i, ] <- colSums(Y[group_ind[[i]], ])
-        }
-        
-        # get matrix that is (p - 1) x J that gives whether or not parameter is zero-comparison
-        zero_comp <- matrix(FALSE, nrow = n_groups - 1, J)
-        for (i in 1:(n_groups - 1)) {
-          zero_comp[i, ] <- (group_counts[1, ] == 0) * (group_counts[i + 1, ] == 0) == 1
-        }
-        
-        # if there are any zero-comparison parameters
-        if (sum(zero_comp) > 0) {
-          cov <- colnames(base_X)
-          cat <- colnames(Y)
-          new_zero_comp_dat <- data.frame(covariate = rep(cov, each = length(cat)),
-                                      category = rep(cat, length(cov)))
-          new_zero_comp_dat$zero_comparison <- as.vector(t(zero_comp))
-          zero_comp_dat <- rbind(zero_comp_dat, new_zero_comp_dat)
+        if (all_0_1 & sum_0_or_1) {
+          # get indices for each group 
+          n_groups <- ncol(base_X) + 1
+          group_ind <- vector(mode = "list", length = n_groups)
+          group_ind[[1]] <- which(rowSums(base_X) == 0)
+          
+          for (i in 1:ncol(base_X)) {
+            group_ind[[i + 1]] <- which(base_X[, i] == 1)
+          }
+          
+          # get Y sums for each group 
+          J <- ncol(Y)
+          group_counts <- matrix(NA, nrow = n_groups, ncol = J)
+          for (i in 1:n_groups) {
+            group_counts[i, ] <- colSums(Y[group_ind[[i]], ])
+          }
+          
+          # get matrix that is (p - 1) x J that gives whether or not parameter is zero-comparison
+          zero_comp <- matrix(FALSE, nrow = n_groups - 1, J)
+          for (i in 1:(n_groups - 1)) {
+            zero_comp[i, ] <- (group_counts[1, ] == 0) * (group_counts[i + 1, ] == 0) == 1
+          }
+          
+          # if there are any zero-comparison parameters
+          if (sum(zero_comp) > 0) {
+            cov <- colnames(base_X)
+            cat <- colnames(Y)
+            new_zero_comp_dat <- data.frame(covariate = rep(cov, each = length(cat)),
+                                            category = rep(cat, length(cov)))
+            new_zero_comp_dat$zero_comparison <- as.vector(t(zero_comp))
+            zero_comp_dat <- rbind(zero_comp_dat, new_zero_comp_dat)
+          }
         }
       }
       if (nrow(zero_comp_dat) > 0) {
