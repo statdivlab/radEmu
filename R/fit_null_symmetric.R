9e3a5b5f96b6a3f97df024ca1a90c8c95863dd69
Sarah Teichman <teichs@uw.edu>
2025-05-19 12:02:44 -0700

    integrate davids updates from pr 144

--- R/fit_null_symmetric.R
+++ R/fit_null_symmetric.R
@@ -56,43 +56,285 @@ fit_null_symmetric <- function(B,
                                trackB = FALSE 
 ) {
   
-  ### David to replace - for now redirecting to fit_null so that everything still runs! 
-  res <- fit_null(B = B,
-                  Y = Y, 
-                  X = X, 
-                  X_cup = X_cup,
-                  k_constr = k_constr, 
-                  j_constr = j_constr, 
-                  j_ref = j_ref, 
-                  constraint_fn = constraint_fn, 
-                  constraint_grad_fn = constraint_grad_fn, 
-                  rho_init = rho_init, 
-                  tau = tau, 
-                  kappa = kappa, 
-                  B_tol = B_tol, 
-                  inner_tol = inner_tol, 
-                  constraint_tol = constraint_tol, 
-                  max_step = max_step, 
-                  c1 = c1, 
-                  maxit = maxit, 
-                  inner_maxit = inner_maxit, 
-                  verbose = verbose, 
-                  trackB = trackB)
-  B <- res$B
-  iter <- res$niter
-  gap <- res$gap
-  u <- res$u
-  rho <- res$rho
-  Bs <- res$Bs
-  ###
+  if(is.list(constraint_fn)){
+    constraint_fn <- constraint_fn[[k_constr]]
+    constraint_grad_fn <- constraint_grad_fn[[k_constr]]
+  }
+  #useful constants
+  p <- ncol(X)
+  n <- nrow(Y)
+  J <- ncol(Y)
   
-  return(list("B" = B,
-              "k_constr" = k_constr,
-              "j_constr" = j_constr,
-              "niter" = iter,
-              "gap" = gap,
-              "u" = u,
-              "rho" = rho,
-              "Bs" = Bs))
+  #reparametrize if convenience constraint taxon is also taxon containing null-constrained param
+  if(j_ref == j_constr){
+    arch_j_ref <- j_ref
+    new_j_ref <- which.max(colSums(Y>1)[-j_ref])
+    new_j_ref <- new_j_ref + as.numeric(new_j_ref>=j_ref)
+    j_ref <- new_j_ref
+  } else{
+    arch_j_ref <- NULL
+  }
+  
+  #impose convenience constraint
+  for(k in 1:p){
+    B[k,] <- B[k,] - B[k,j_ref]
+  }
+  
+  #force B to satisfy null constraint
+  #(this is the bit where symmetry of g() is important)
+  B[k_constr,j_constr] <- constraint_fn(B[k_constr,-j_constr])
+  
+  
+  #update z
+  z <- update_z(Y,X,B)
+  
+  #create df for tracking B through optimization if trackB = TRUE
+  if(trackB){
+    df <- data.frame(iter = numeric(0),
+                     iter_j = numeric(0),
+                     k = numeric(0),
+                     j = numeric(0),
+                     value = numeric(0),
+                     ll = numeric(0),
+                     gr_norm = numeric(0))
+  }
+  
+  #break taxa, j_ref and j_constr excluded, into two groups 
+  #according to whether they are above or below B[k_constr,j_constr]
+  #and order these groups so each set begins with j s.t. B[k_constr,j] is
+  #closest to B[k_constr,j_constr]
+  
+  above_g <- setdiff(which(B[k_constr,] > constraint_fn(B[k_constr,])),c(j_ref,j_constr))
+  above_g <- above_g[order(B[k_constr,above_g])]
+  below_g <- setdiff(which(B[k_constr,] < constraint_fn(B[k_constr,])),c(j_ref,j_constr))
+  below_g <- below_g[order(-B[k_constr,below_g])]
+  
+  #count total pairings between taxa in above_g and below_g, and count remaining taxa
+  npairs <- min(length(above_g),length(below_g))
+  nsingles <- max(length(above_g),length(below_g)) - npairs
+  
+  #now create pairs sandwiching B[k_constr,j_constr] and put remaining taxa into 'singles'
+  if(npairs >0){
+    pairs <- cbind(above_g[1:npairs],below_g[1:npairs])
+    singles <- c(above_g[-(1:npairs)],below_g[-(1:npairs)])
+  } else{
+    pairs <- integer(0)
+    singles <- c(above_g,below_g)
+  }
+  
+  
+  #compute fitted log means
+  log_means <- X%*%B
+  for(i in 1:n){
+    log_means[i,] <- log_means[i,] + z[i]
+  }
+  
+  #compute ll
+  ll <- sum(Y*log_means - exp(log_means))
+  
+  #initialize 'keep_going' indicator for continuing optimization as well as 'iter' 
+  #to track optimization iterations
+  keep_going <- TRUE
+  iter <- 1
+  
+  # temporarily set use_optim to `TRUE` always
+  use_optim <- TRUE
+  
+  #iterate until something happens
+  while(keep_going){
+    prev_B <- B
+    prev_ll <- ll
+    
+    #loop through paired above_g and below_g taxa, and then through remaining singletons
+    for(jset in 1:(npairs + nsingles)){
+      
+      
+      #choose groups of taxa to optimize over (in addition to j_constr, which is always included)
+      if(jset > npairs){
+        
+        js <- singles[jset - npairs]
+      } else{
+        js <- pairs[jset,]
+      }
+      
+      njs <- length(js)
+      
+      #computed update for js and j_constr
+      if(use_optim){
+        
+        update <- optim(rep(0,p*njs + p - 1),
+                        function(x) -null_repar_ll(x = x,js = js,
+                                                   B = B,
+                                                   z = z,
+                                                   p = p,
+                                                   Y = Y,
+                                                   X = X,
+                                                   j_constr = j_constr,
+                                                   k_constr = k_constr,
+                                                   constraint_fn = constraint_fn),
+                        # gr = function(x) -null_repar_ll_gr(x = x,
+                        #                                    js = js,
+                        #                                    B = B,
+                        #                                    z = z,
+                        #                                    p = p,
+                        #                                    Y = Y,
+                        #                                    X = X,
+                        #                                    j_constr = j_constr,
+                        #                                    k_constr = k_constr,
+                        #                                    constraint_fn = constraint_fn,
+                        #                                    constraint_grad_fn = constraint_grad_fn),                        
+                        method = "L-BFGS-B",
+                        control = list(maxit = 10),
+                        lower = -1,
+                        upper = 1)
+        
+        #unfortunately optim sometimes returns 'optimal' parameter values that 
+        #decrease the likelihood relative to starting parameters, so we have to 
+        #check that this has not occurred
+        #(why, optim?)
+        if(-update$value > null_repar_ll(x = rep(0,p*njs + p - 1),
+                                         js = js,
+                                         B = B,
+                                         z = z,
+                                         p = p,
+                                         Y = Y,
+                                         X = X,
+                                         j_constr = j_constr,
+                                         k_constr = k_constr,
+                                         constraint_fn = constraint_fn)){
+          #update elements of B corresponding to j in js
+          for(jind in 1:njs){
+            B[,js[jind]] <- B[,js[jind]] + update$par[1:p + (jind - 1)*p]
+          }
+          #update unconstrained elements of B[,j_constr]
+          B[-k_constr,j_constr] <- B[-k_constr,j_constr] + update$par[njs*p + 1:(p - 1)]
+          #and update constrained element
+          B[k_constr,j_constr] <- constraint_fn(B[k_constr,-j_constr])
+          
+        } else{
+          if(verbose){
+            message("Update for j = ", pairs[npairs,1]," and ", pairs[npairs,2], " did not increase log likelihood. Update skipped.")
+          }
+        }
+      } else{
+        #it would almost certainly be faster to solve the above subproblem via fisher scoring
+        #but I have not implemented this
+        stop("Fisher scoring not implemented yet")
+        
+      }
+      
+      if(trackB){
+        gr <- get_constrained_gr(Y = Y,
+                                 X = X,
+                                 B = B,
+                                 z = z,
+                                 js = setdiff(1:J,c(j_constr,j_ref)),
+                                 j_constr = j_constr,
+                                 k_constr = k_constr,
+                                 j_ref = j_ref,
+                                 constraint_grad_fn = constraint_grad_fn,
+                                 gr_only = TRUE)
+        
+        df <-   rbind(df,
+                      data.frame(iter = iter,
+                                 k = rep(1:p,each = length(js) + 1),
+                                 j = rep(c(js,j_constr),p),
+                                 value = do.call(c,lapply(1:p,function(k) B[k,c(js,j_constr)])),
+                                 ll =   -ll ,
+                                 gr_norm = sum(gr^2)))
+      }
+      
+      #update z
+      z <- update_z(Y = Y, X = X, B = B)
+    }
+    
+    #compute ll again
+    log_means <- X%*%B
+    for(i in 1:nrow(Y)){
+      log_means[i,] <- log_means[i,] + z[i]
+    }
+    
+    ll <- sum(Y*log_means - exp(log_means))
+    
+    #compute gradient of ll
+    
+    
+    #say things if needed
+    if(verbose){
+      #compute gradient if we didn't already
+      if(!trackB){
+        gr <- get_constrained_gr(Y = Y,
+                                 X = X,
+                                 B = B,
+                                 z = z,
+                                 js = setdiff(1:J,c(j_constr,j_ref)),
+                                 j_constr = j_constr,
+                                 k_constr = k_constr,
+                                 j_ref = j_ref,
+                                 constraint_grad_fn = constraint_grad_fn,
+                                 gr_only = TRUE)
+      }
+      #tell you all about the ll, gradient, and how much B has changed this loop
+      message("ll = ", round(ll,1))
+      message("ll increased by ",round(ll - prev_ll,1))
+      message("gr_norm = ",round(sum(gr^2),1))
+      message("max abs diff in B = ",signif(max(abs(B - prev_B)),2))}
+    
+    #recompute above_g and below_g
+    above_g <- setdiff(which(B[k_constr,] > constraint_fn(B[k_constr,])),c(j_ref,j_constr))
+    above_g <- above_g[order(B[k_constr,above_g])]
+    below_g <- setdiff(which(B[k_constr,] < constraint_fn(B[k_constr,])),c(j_ref,j_constr))
+    below_g <- below_g[order(-B[k_constr,below_g])]
+    
+    #and pairs of taxa and singleton taxa to optimize over iteratively
+    npairs <- min(length(above_g),length(below_g))
+    nsingles <- max(length(above_g),length(below_g)) - npairs
+    
+    if(npairs >0){
+      pairs <- cbind(above_g[1:npairs],below_g[1:npairs])
+      singles <- c(above_g[-(1:npairs)],below_g[-(1:npairs)])
+    } else{
+      pairs <- integer(0)
+      singles <- c(above_g,below_g)
+    }
+    
+    #increment iteration counter
+    iter <- iter + 1
+    
+    #determine whether we should stop iterating 
+    if(iter > maxit){
+      keep_going <- FALSE
+      converged <- FALSE
+    }
+    
+    if(max(abs(B - prev_B)) < B_tol){
+      keep_going <- FALSE
+      converged <- TRUE
+    }
+    
+  }
+  
+  #if we changed j_ref, change it back
+  if(!is.null(arch_j_ref)){
+    for(k in 1:p){
+      B[k,] <- B[k,] - B[k,arch_j_ref]
+    }
+  }
+  
+  #create something to return if trackB is FALSE
+  if(!trackB){
+    df = NULL
+  }
+  #return a list containing B 'n' fixin's 
+  return(list(B = B,
+              k_constr = k_constr,
+              j_constr = j_constr,
+              niter = iter,
+              converged = converged,
+              Bs = df,
+              gap = 0, #gap is zero by parametrization
+              "u" = NA,
+              "rho" = NA
+  ))
 }
 
