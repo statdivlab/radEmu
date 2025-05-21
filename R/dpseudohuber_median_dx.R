dd2b96e5c7e6f50d5fad569aba8b19c77a230fa9
Sarah Teichman <teichs@uw.edu>
2025-05-21 12:23:17 -0700

    add na.rm behavior to pseudohuber median and make small fix to micro wald test

--- R/dpseudohuber_median_dx.R
+++ R/dpseudohuber_median_dx.R
@@ -4,6 +4,10 @@
 #' @param d Smoothing parameter, by default set to \code{0.1}. As \code{d} approaches 
 #' \code{0} the pseudo-Huber median function approaches the median and as \code{d} approaches 
 #' infinity this function approaches the mean. 
+#' @param na.rm Passed to \code{pseudohuber_median}, default is FALSE, if FALSE then when 
+#' \code{x} includes at least one NA value then NA is returned, if TRUE then when \code{x} 
+#' includes at least one NA value then that value is removed and the pseudo-Huber median 
+#' is computed without it.
 #'
 #' @return The derivative of the calculated pseudo-Huber smoothed median over \code{x} with smoothing 
 #' parameter \code{d}. 
@@ -16,15 +20,16 @@
 #' @export
 #'
 dpseudohuber_median_dx <- function(x,
-                                   d = 0.1){
-  ps_center <- pseudohuber_median(x,d)
+                                   d = 0.1,
+                                   na.rm = FALSE){
+  ps_center <- pseudohuber_median(x,d, na.rm = na.rm)
   scaled_sq <- ((x - ps_center)/d)^2
   #derivation of why the following is the derivative of 
   #the pseudohuber centering is given in supplement of 
   #Willis & Clausen (2024)
   w <- sqrt(1/(1 + scaled_sq))
   w3 <- w^3
-  return((w3/sum(w3)))
+  return((w3/sum(w3, na.rm = na.rm)))
 }
 
 #' @rdname dpseudohuber_median_dx
