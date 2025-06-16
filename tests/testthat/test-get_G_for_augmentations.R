d80a2b0debec462f44df2fa5378c6f627f38d6c0
Sarah Teichman <teichs@uw.edu>
2025-06-16 16:51:02 -0700

    update test to show that fast function for G is faster with moderate n and J

--- tests/testthat/test-get_G_for_augmentations.R
+++ tests/testthat/test-get_G_for_augmentations.R
@@ -1,14 +1,14 @@
-test_that("X_cup_from_X_fast works and is faster than X_cup_from_X", {
-  X <- cbind(1, rnorm(20), runif(20))
-  J <- 50
+test_that("get_G_for_augmentations_fast works and is faster than get_G_for_augmentations", {
+  X <- cbind(1, rnorm(200), runif(200))
+  J <- 200
   Xcup2 <- X_cup_from_X_fast(X, J)
   
   start1 <- proc.time()
-  g1 <- get_G_for_augmentations(X, J, 20, Xcup2)
+  g1 <- get_G_for_augmentations(X, J, nrow(X), Xcup2)
   end1 <- proc.time() - start1
   
   start2 <- proc.time()
-  g2 <- get_G_for_augmentations_fast(X, J, 20, Xcup2)
+  g2 <- get_G_for_augmentations_fast(X, J, nrow(X), Xcup2)
   end2 <- proc.time() - start2
   
   expect_true(all.equal(g1, g2))
